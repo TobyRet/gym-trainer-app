5ba660380b5344e44566842fb44924a2bb7f5701
Toby Retallick <toby.retallick@gmail.com>
2025-03-23 23:07:22 +1000

    chore(page) - update app title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <main className="container mx-auto px-4 py-8">
-        <h1 className="text-6xl font-bold mb-6">Gym Trainer</h1>
+        <h1 className="text-xl mb-12">Gym Trainer</h1>
         <WorkoutInterface />
       </main>
     </div>
